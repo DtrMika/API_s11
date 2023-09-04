ed4091f6591301ebdf2de2d63171083173f6ac7e
DtrMika <mikail.dutra.93@gmail.com>
2023-09-04 10:19:13 -0300

    Actualizado

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const container = document.getElementById("contenedor");
 const URL = `https://dog.ceo/api/breeds/image/random`;
+const btnR = document.getElementById("refresh");
 
 fetch(URL)
   .then((res) => res.json())
   .then((data) => {
-    const img = document.getElementById("imgen");
-    img.src = data.message;
-
     const nombreRaza = data.message.split("/")[4];
     const describe = document.getElementById("descripcion");
-    describe.innerHTML = `Raza: <b>${nombreRaza}</b>`;
+    describe.innerHTML = `
+            <div class="card-row" style="width: 30rem;">
+                <img class="card-img-top" src=${data.message} alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Raza: <b>${nombreRaza}</b</p>
+                </div>
+            </div>`;
   });
+
+btnR.addEventListener("click", () => {
+  location.reload();
+});
